cbb1c1943585599d05a369182867e6f7deea6cb7
Lieven <lieven.vaneeckhaute@gmail.com>
2022-10-01 23:00:44 +0200

    introduce build, run test

--- calculate.js
+++ calculate.js
@@ -45,44 +45,7 @@ function has_unfilled_variables(filled_formula) {
     return filled_formula.includes(" ");
 }
 
-function calculateAll(field_definitions) {
-    let a = document.getElementById("a").value;
-    let c = document.getElementById("c").value;
-    let e = document.getElementById("e").value;
-    let h = document.getElementById("h").value;
-
-    let p = document.getElementById("p").value;
-    let µ = document.getElementById("µ").value;
-    let M = document.getElementById("M").value;
-
-    let pos0 = document.getElementById("pos0").value;
-    let pos1 = document.getElementById("pos1").value;
-    let pos2 = document.getElementById("pos2").value;
-
-    let v0 = document.getElementById("v0").value;
-    let v1 = document.getElementById("v1").value;
-    let v2 = document.getElementById("v2").value;
-
-    let h0 = document.getElementById("h0").value;
-    let h1 = document.getElementById("h1").value;
-    let h2 = document.getElementById("h2").value;
-
-    let v_old = document.getElementById("v").value;
-    let r = document.getElementById("r").value;
-
-    
-    let apogee = document.getElementById("apogee").value;
-    let perigee = document.getElementById("perigee").value;
-    let angle_r_v = document.getElementById("angle_r_v").value;
-
-    const allElements = document.getElementsByTagName('input');
-    var $ = {}
-    var v = {}
-    for (const element of allElements) {
-        $[element.id] = element;
-        v[element.id] = element.value;
-      }
-
+/*
     if (v0 !== "" && v1 !== "" &&v2 !== "" && pos0 !== "" && pos1 !== "" && pos2 !== "") {
         h0 = pos1 * v2 - pos2 * v1;
         h1 = pos2 * v0 - pos0 * v2;
@@ -91,8 +54,7 @@ function calculateAll(field_definitions) {
         document.getElementById("h1").value = h1;
         document.getElementById("h2").value = h2;
     }
-
-}
+*/
 
 
 function addVectorElement(colElement, field_definition,field_definitions) {
--- calculate.test.js
+++ calculate.test.js
@@ -1,12 +1,7 @@
 const [calculateAllWithFields, formula_has_solution] = require('./calculate');
 
 test('calculateAllWithFields', () => {
-    var cObject = {};
-    calculateAllWithFields([ {id:"c", placeholder: "distance to the focus", label: "c", formulas:[" e * a "]}],{c:cObject}, {e:1, a:2},
-    );
+    var cObject = {value:""};
+    calculateAllWithFields([ {id:"c", placeholder: "distance to the focus", label: "c", formulas:[" e * a "]}],{c:cObject}, {e:1, a:2});
     expect(cObject.value).toBe(1*2);
-})
-
-test('formula_has_solution', () => {
-    expect(formula_has_solution(" e * a ", {e:1, a:2})).toBe("1*2");
 })
